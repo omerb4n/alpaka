09d4fa64035cec654966c0151849aa905ade3846
omerb <a@b.com>
2020-03-25 15:06:18 +0200

    Move PackageInfo class to package_info.py

--- alpaka/apk/apk_info.py
+++ alpaka/apk/apk_info.py
@@ -1,34 +1,9 @@
 from typing import Optional, List
 
 from alpaka.apk.analyzed_apk import AnalyzedApk
+from alpaka.apk.package_info import PackageInfo
 from alpaka.obfuscation.obfuscation import PackageNameObfuscationDetector
 from alpaka.utils import filter_dict
-
-
-class PackageInfo:
-    NAME_SEPARATOR = '/'
-
-    def __init__(self, package_name_prefix: str, is_obfuscated_name):
-        self.name_prefix = package_name_prefix
-        self.classes = []
-        self.is_obfuscated_name = is_obfuscated_name
-
-    def add_class(self, class_name: str):
-        self.classes.append(class_name)
-
-    def get_classes(self):
-        return self.classes
-
-    @staticmethod
-    def get_parent_package_name_prefix(package_name_prefix):
-        return package_name_prefix[:package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR)]
-
-    @staticmethod
-    def get_package_name(package_name_prefix):
-        return package_name_prefix[package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR) + 1:]
-
-    def __repr__(self):
-        return "~Obfuscated" if self.is_obfuscated_name else ""
 
 
 class ApkInfo:
---
+++ alpaka/apk/package_info.py
@@ -0,0 +1,24 @@
+class PackageInfo:
+    NAME_SEPARATOR = '/'
+
+    def __init__(self, package_name_prefix: str, is_obfuscated_name):
+        self.name_prefix = package_name_prefix
+        self.classes = []
+        self.is_obfuscated_name = is_obfuscated_name
+
+    def add_class(self, class_name: str):
+        self.classes.append(class_name)
+
+    def get_classes(self):
+        return self.classes
+
+    @staticmethod
+    def get_parent_package_name_prefix(package_name_prefix):
+        return package_name_prefix[:package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR)]
+
+    @staticmethod
+    def get_package_name(package_name_prefix):
+        return package_name_prefix[package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR) + 1:]
+
+    def __repr__(self):
+        return "~Obfuscated" if self.is_obfuscated_name else ""
