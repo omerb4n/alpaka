25f5f32a31e400206550c1fde9c70f8707d9a34b
omerb <a@b.com>
2020-03-25 15:07:58 +0200

    Move NAME_SEPERATOR outside of PackageInfo

--- alpaka/apk/package_info.py
+++ alpaka/apk/package_info.py
@@ -1,5 +1,7 @@
+NAME_SEPARATOR = '/'
+
+
 class PackageInfo:
-    NAME_SEPARATOR = '/'
 
     def __init__(self, package_name_prefix: str, is_obfuscated_name):
         self.name_prefix = package_name_prefix
@@ -14,11 +16,11 @@ class PackageInfo:
 
     @staticmethod
     def get_parent_package_name_prefix(package_name_prefix):
-        return package_name_prefix[:package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR)]
+        return package_name_prefix[:package_name_prefix.rfind(NAME_SEPARATOR)]
 
     @staticmethod
     def get_package_name(package_name_prefix):
-        return package_name_prefix[package_name_prefix.rfind(PackageInfo.NAME_SEPARATOR) + 1:]
+        return package_name_prefix[package_name_prefix.rfind(NAME_SEPARATOR) + 1:]
 
     def __repr__(self):
         return "~Obfuscated" if self.is_obfuscated_name else ""
