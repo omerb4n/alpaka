eefadd7e6dd3bbdd9b94ca0cb2498330d86f78b8
TulkinRB <tulkinrb@gmail.com>
2020-03-26 22:22:33 +0200

    Finish the signature calculator skeleton

--- alpaka/class_signature/class_signature_calculator.py
+++ alpaka/class_signature/class_signature_calculator.py
@@ -1,34 +1,45 @@
 from androguard.core.analysis.analysis import ClassAnalysis
 
+from alpaka.class_signature.signature import ClassSignature
+
 
 class ClassSignatureCalculator:
-    def __init__(self, class_analysis):
-        self._class_analysis: ClassAnalysis = class_analysis
 
-    def calculate(self):
-        """
+    def calculate_class_signature(self, class_analysis: ClassAnalysis) -> ClassSignature:
+        return ClassSignature(
+            member_count=self._get_member_count(class_analysis),
+            method_count=self._get_method_count(class_analysis),
+            instructions_count=self._get_instructions_count(class_analysis),
+            members_simhash=self._calc_members_simhash(class_analysis),
+            methods_params_simhash=self._calc_methods_params_simhash(class_analysis),
+            method_returns_simhash=self._calc_methods_returns_simhash(class_analysis),
+            instructions_simhash=self._calc_instructions_simhash(class_analysis),
+        )
 
-        :return: ClassSignature
-        """
-        pass
-
-    def get_member_count(self):
+    @classmethod
+    def _get_member_count(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_method_count(self):
+    @classmethod
+    def _get_method_count(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_instructions_count(self):
+    @classmethod
+    def _get_instructions_count(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_members_simhash(self):
+    @classmethod
+    def _calc_members_simhash(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_methods_params_simhash(self):
+    @classmethod
+    def _calc_methods_params_simhash(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_methods_returns_simhash(self):
+    @classmethod
+    def _calc_methods_returns_simhash(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
 
-    def get_instructions_simhash(self):
+    @classmethod
+    def _calc_instructions_simhash(cls, class_analysis: ClassAnalysis):
         raise NotImplementedError()
