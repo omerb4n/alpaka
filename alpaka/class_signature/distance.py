c4848eafb89e05a12fb5f50586c5a76743d97c4d
TulkinRB <tulkinrb@gmail.com>
2020-03-26 22:09:16 +0200

    Move the signature classes to the package

--- alpaka/class_signature/distance.py
+++ alpaka/class_signature/distance.py
@@ -1,26 +1,6 @@
-from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 
-
-class ClassSignature:
-    def __init__(
-            self,
-            member_count: int,
-            method_count: int,
-            instructions_count: int,
-            members_simhash: str,
-            methods_params_simhash: str,
-            method_returns_simhash: str,
-            instructions_simhash: str,
-    ):
-        self.member_count = member_count
-        self.method_count = method_count
-        self.instructions_count = instructions_count
-        self.members_simhash = members_simhash
-        self.methods_params_simhash = methods_params_simhash
-        self.methods_returns_simhash = method_returns_simhash
-        self.instructions_simhash = instructions_simhash
-
+from alpaka.class_signature.signature import ClassSignature
 
 
 class SignatureDistanceCalculator(metaclass=ABCMeta):
