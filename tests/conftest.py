f6cf443e6217b7211d974e54f1d340cf6be15421
omerb <a@b.com>
2020-03-29 20:48:31 +0300

    Add test fixtures for hello.apk

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,25 @@
 import os
 
+import pytest
+
+from alpaka.analyzed_apk import AnalyzedApk
+
 TEST_FILES_PATH = os.path.join(os.path.dirname(__file__), "test_files")
 APKS_PATH = os.path.join(TEST_FILES_PATH, "apks")
 AG_SESSIONS_PATH = os.path.join(TEST_FILES_PATH, "ag_sessions")
+
+TOAST_APKS_PATH = os.path.join(APKS_PATH, 'toastAPK')
+TOAST_HELLO_APK_PATH = os.path.join(TOAST_APKS_PATH, 'hello.apk')
+TOAST_HELLO_SESSION_PATH = os.path.join(AG_SESSIONS_PATH, 'hello.ag')
+TOAST_BYE_APK_PATH = os.path.join(TOAST_APKS_PATH, 'bye.apk')
+TOAST_BYE_SESSION_PATH = os.path.join(AG_SESSIONS_PATH, 'bye.ag')
+
+
+@pytest.fixture(scope='session')
+def hello_analyzed_apk_fixture() -> AnalyzedApk:
+    return AnalyzedApk(TOAST_HELLO_APK_PATH, session_path=TOAST_HELLO_SESSION_PATH)
+
+
+@pytest.fixture(scope='session')
+def hello_apk_classes_fixture(hello_analyzed_apk_fixture):
+    return hello_analyzed_apk_fixture.analysis.classes
