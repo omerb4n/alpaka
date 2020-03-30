7f504bd45c7194a547b1621aa2f120ddfbf7383f
omerb <a@b.com>
2020-03-30 16:54:59 +0300

    Add calculate_distance

--- alpaka/class_signature/simhash_utils.py
+++ alpaka/class_signature/simhash_utils.py
@@ -8,3 +8,7 @@ def calculate_simhash(tokens: Iterable[str]) -> int:
         simhash.unsigned_hash(bytes(token, 'utf-8'))
         for token in tokens
     ))
+
+
+def calculate_distance(simhash1: int, simhash2: int):
+    return simhash.num_differing_bits(simhash1, simhash2)
