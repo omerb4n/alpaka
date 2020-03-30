188c8be0490204e045e73bc919f2c53074fb410f
omerb <a@b.com>
2020-03-30 17:31:54 +0300

    Add test_calc_instructions_simhash

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
+from typing import Type, Any, Callable
+from unittest.mock import Mock
 
 import pytest
+from _pytest.monkeypatch import MonkeyPatch
 
 from alpaka.analyzed_apk import AnalyzedApk
 
@@ -13,6 +16,11 @@ TOAST_HELLO_APK_PATH = os.path.join(TOAST_APKS_PATH, 'hello.apk')
 TOAST_HELLO_SESSION_PATH = os.path.join(AG_SESSIONS_PATH, 'hello.ag')
 TOAST_BYE_APK_PATH = os.path.join(TOAST_APKS_PATH, 'bye.apk')
 TOAST_BYE_SESSION_PATH = os.path.join(AG_SESSIONS_PATH, 'bye.ag')
+
+
+def mock_function(monkeypatch: MonkeyPatch, target: Type[Any], function: Callable, side_effect: Callable = None):
+    mock = Mock(side_effect=side_effect)
+    monkeypatch.setattr(target, function.__name__, mock)
 
 
 @pytest.fixture(scope='session')
