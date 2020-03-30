c8d0fbb5b613b652cd35df30365b77c4c4eb57d1
omerb <a@b.com>
2020-03-31 02:28:12 +0300

    Refactor ClassMatcher - divided functions

--- alpaka/matchers/class_matcher.py
+++ alpaka/matchers/class_matcher.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from alpaka.apk.apk_info import ApkInfo
 from alpaka.apk.class_info import ClassInfo
@@ -11,24 +11,34 @@ class ClassMatcher:
         self._old_apk_info = old_apk_info
         self._new_apk_info = new_apk_info
         self._classes_pool_matcher = ClassesPoolMatcher(self._old_apk_info, self._new_apk_info)
+        self.class_matches = []
 
     def find_classes_matches(self) -> List[ClassMatches]:
         classes_matches: List[ClassMatches] = []
         for classes_pool_match in self._classes_pool_matcher.pop_matched_packages_classes_pools():
-            for class_key in classes_pool_match.old_classes_pool:
-                new_class = classes_pool_match.new_classes_pool.get(class_key)
-                if new_class.is_obfuscated_name:
-                    pass
-                else:
-                    old_class = classes_pool_match.old_classes_pool.pop(class_key)
-                    del classes_pool_match.new_classes_pool[class_key]
-                    classes_matches.append(ClassMatches(old_class, [new_class]))
+            self._find_classes_matches_by_name(classes_pool_match.old_classes_pool, classes_pool_match.new_classes_pool)
         all_classes_pool = self._classes_pool_matcher.get_all_classes_pool()
         for class_info in all_classes_pool.old_classes_pool:
             # TODO: uncomment the line bellow
             # classes_matches.append(self._find_class_matches_by_signature(class_info, classes_pool.new_classes))
             pass
         return classes_matches
+
+    @staticmethod
+    def _find_class_match_by_name(old_classes_pool: dict, new_classes_pool: dict, class_key) -> Optional[ClassMatch]:
+        new_class: ClassInfo = new_classes_pool.get(class_key)
+        if new_class.is_obfuscated_name:
+            return None
+        else:
+            del old_classes_pool[class_key]
+            del new_classes_pool[class_key]
+            return ClassMatch(new_class, 1.0)
+
+    def _find_classes_matches_by_name(self, old_classes_pool: dict, new_classes_pool: dict):
+        for class_key in old_classes_pool:
+            class_match = self._find_class_match_by_name(old_classes_pool, new_classes_pool, class_key)
+            if class_match:
+                self.class_matches.append(ClassMatches(old_classes_pool[class_key], [class_match]))
 
     def _find_class_matches_by_signature(self, class_to_match: ClassInfo,
                                          potential_matches: List[ClassInfo]) -> ClassMatches:
