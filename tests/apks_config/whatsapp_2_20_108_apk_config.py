fb6f16a6976be90f0ee2b8c4b2d4b9800c883a37
omerb <a@b.com>
2020-04-02 14:06:55 +0300

    Add test_whatsapp.py

--- tests/apks_config/whatsapp_2_20_108_apk_config.py
+++ tests/apks_config/whatsapp_2_20_108_apk_config.py
@@ -3,7 +3,7 @@ import os
 from tests.apks_config.whatsapp_apks_config import WHATSAPP_APKS_PATH
 from tests.conftest import AG_SESSIONS_PATH
 
-APK_NAME = 'com.whatsapp_2.20.108-453352_minAPI15(armeabi-v7a)(nodpi)_apkmirror.com.apk'
+APK_NAME = 'com.whatsapp_2.20.108-453352_minAPI15(armeabi-v7a)(nodpi)_apkmirror.com'
 
 WHATSAPP_2_20_108_APK_PATH = os.path.join(WHATSAPP_APKS_PATH, f'{APK_NAME}.apk')
 WHATSAPP_2_20_108_SESSION_PATH = os.path.join(AG_SESSIONS_PATH, f'{APK_NAME}.ag')
