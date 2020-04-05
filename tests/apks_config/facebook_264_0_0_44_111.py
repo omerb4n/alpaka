6b2c616656f1564983208ca2930993dcd7b98cc5
omerb <a@b.com>
2020-04-06 01:34:51 +0300

    Add facebook apk configs

---
+++ tests/apks_config/facebook_264_0_0_44_111.py
@@ -0,0 +1,5 @@
+from tests.apks_config.apk_config import ApkConfig
+from tests.apks_config.facebook_apks_config import FACEBOOK_APKS_PATH
+
+APK_NAME = 'com.facebook.katana_264.0.0.44.111-206636685_minAPI16(armeabi-v7a)(120,160dpi)_apkmirror.com'
+FACEBOOK_264_0_0_44_111_APK_CONFIG = ApkConfig(FACEBOOK_APKS_PATH, APK_NAME)
