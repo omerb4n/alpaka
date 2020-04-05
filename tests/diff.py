71153f350d56ec14f78d4e28ef5acb6e3f159392
omerb <a@b.com>
2020-04-05 14:42:21 +0300

    Chnaged diff function signature - receive ApkConfig

--- tests/diff.py
+++ tests/diff.py
@@ -2,12 +2,12 @@ from typing import Callable
 
 from alpaka.apk.analyzed_apk import AnalyzedApk
 from alpaka.apk_differ import ApkDiffer
+from tests.apks_config.apk_config import ApkConfig
 
 
-def diff(old_apk_path: str, old_apk_session_path: str, new_apk_path: str, new_apk_session_path: str,
-         class_filter: Callable = None):
-    old_apk = AnalyzedApk(old_apk_path, session_path=old_apk_session_path)
-    new_apk = AnalyzedApk(new_apk_path, session_path=new_apk_session_path)
+def diff(old_apk_config: ApkConfig, new_apk_config: ApkConfig, class_filter: Callable = None):
+    old_apk = AnalyzedApk(old_apk_config.apk_path, session_path=old_apk_config.session_path)
+    new_apk = AnalyzedApk(new_apk_config.apk_path, session_path=new_apk_config.session_path)
 
     apk_differ = ApkDiffer(old_apk, new_apk)
     if class_filter:
--- tests/test_whatsapp.py
+++ tests/test_whatsapp.py
@@ -1,5 +1,3 @@
-import threading
-
 from androguard.core.analysis.analysis import ClassAnalysis
 
 from tests.apks_config.whatsapp_2_20_108_apk_config import WHATSAPP_2_20_108_APK_CONFIG
@@ -15,7 +13,4 @@ def filter_whatsapp_classes(class_name_prefix: str, _class_analysis: ClassAnalys
 
 
 def test_whatsapp_apk():
-    threading.stack_size(50000)
-    diff(WHATSAPP_2_20_89_APK_CONFIG.apk_path, WHATSAPP_2_20_89_APK_CONFIG.session_path,
-         WHATSAPP_2_20_108_APK_CONFIG.apk_path, WHATSAPP_2_20_108_APK_CONFIG.session_path,
-         filter_whatsapp_classes)
+    diff(WHATSAPP_2_20_89_APK_CONFIG, WHATSAPP_2_20_108_APK_CONFIG, filter_whatsapp_classes)
