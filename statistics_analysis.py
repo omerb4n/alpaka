c246c60a88fd8f4542d117c044bad117cd411264
TulkinRB <tulkinrb@gmail.com>
2020-04-26 18:56:53 +0300

    Add mean calculation

--- statistics_analysis.py
+++ statistics_analysis.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from matplotlib import pyplot
 
 
-def main(raw_results_file_paths, analyzed_data_csv_path, result_charts_dir):
+def main(raw_results_file_paths, analyzed_data_csv_path, result_charts_dir, calc_mean):
     correct_distance_counts_per_parameter = calc_correct_distance_counts_per_parameter(raw_results_file_paths)
     all_distance_counts_per_parameter = calc_all_distance_counts_per_parameter(raw_results_file_paths)
     incorrect_distance_counts_per_parameter = calc_incorrect_distance_counts_per_parameter(correct_distance_counts_per_parameter, all_distance_counts_per_parameter)
@@ -23,6 +23,16 @@ def main(raw_results_file_paths, analyzed_data_csv_path, result_charts_dir):
             plot_graph_from_dict(all_distance_counts, f'{param_name}_all_distances', result_charts_dir)
         for param_name, incorrect_distance_counts in incorrect_distance_counts_per_parameter.items():
             plot_graph_from_dict(incorrect_distance_counts, f'{param_name}_incorrect_distances', result_charts_dir)
+    if calc_mean:
+        mean_output_lines = []
+        for param_name, correct_distance_counts in correct_distance_counts_per_parameter.items():
+            mean_output_lines.append(f'{param_name}_correct: {calc_mean_from_histogram(correct_distance_counts)}')
+        for param_name, all_distance_counts in all_distance_counts_per_parameter.items():
+            mean_output_lines.append(f'{param_name}_all: {calc_mean_from_histogram(all_distance_counts)}')
+        for param_name, incorrect_distance_counts in incorrect_distance_counts_per_parameter.items():
+            mean_output_lines.append(f'{param_name}_incorrect: {calc_mean_from_histogram(incorrect_distance_counts)}')
+        for line in sorted(mean_output_lines):
+            print(line)
 
 
 def calc_correct_distance_counts_per_parameter(raw_results_file_paths):
@@ -60,6 +70,17 @@ def calc_incorrect_distance_counts_per_parameter(correct_distance_counts_per_par
     return incorrect_distance_counts_per_parameter
 
 
+def calc_mean_from_histogram(histogram):
+    weighted_sum = 0
+    total_count = 0
+    for value, count in histogram.items():
+        if value is None:
+            continue
+        weighted_sum += int(value) * count
+        total_count += count
+    return weighted_sum/float(total_count)
+
+
 def plot_graph_from_dict(dct, graph_name, results_dir):
     dct = dict(dct)
     dct.pop(None, None)
@@ -84,6 +105,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-c', '--csv-path', dest='analyzed_data_csv_path')
     parser.add_argument('-p', '--plotted-charts-dir', dest='result_charts_dir')
+    parser.add_argument('-m', '--calc-mean', dest='calc_mean', action='store_true')
     parser.add_argument('raw_result_files', nargs='+')
     args = parser.parse_args()
-    main(args.raw_result_files, args.analyzed_data_csv_path, args.result_charts_dir)
+    main(args.raw_result_files, args.analyzed_data_csv_path, args.result_charts_dir, args.calc_mean)
