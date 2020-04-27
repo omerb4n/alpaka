6c3e5c8f0f4939f89c1bed6ee901569ea2d33e50
TulkinRB <tulkinrb@gmail.com>
2020-04-27 10:58:27 +0300

    Fix missing items() call

--- alpaka/encoders/classes_matches_encoder.py
+++ alpaka/encoders/classes_matches_encoder.py
@@ -7,7 +7,7 @@ from alpaka.matching.base import Match
 def convert_class_matches_dict_to_output_format(matches_dict: Mapping[str, Iterable[Match[ClassInfo]]]):
     return {
         class_name: _convert_matches_to_output_format(class_matches)
-        for class_name, class_matches in matches_dict
+        for class_name, class_matches in matches_dict.items()
     }
 
 
